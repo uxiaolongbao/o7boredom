3f06a55f0bd958be9dfa6e18495600094ea50cf3
uxiaolongbao <urisonxiao@gmail.com>
2025-04-06 18:24:56 -0700

    updated jumping mechanics

--- interesting name/Assets/Scripts/PlayerMove.cs
+++ interesting name/Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private bool grounded;
 
     private void Awake()
     {
@@ -13,9 +14,29 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
-        body.linearVelocity = new UnityEngine.Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocityY);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new UnityEngine.Vector2(horizontalInput * speed, body.linearVelocityY);
 
-        if(Input.GetKey(KeyCode.UpArrow))
-            body.linearVelocity = new UnityEngine.Vector2(body.linearVelocityX, speed/5);
+        //flip player when moving left and right (for animations/sprites) + movement
+        if(horizontalInput > 0.01f)
+            transform.localScale = UnityEngine.Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new UnityEngine.Vector3(-1, 1, 1);
+
+        if(Input.GetKey(KeyCode.UpArrow) && grounded)
+            Jump();
+    }
+
+    //simplifying jumping and all its conditions
+    private void Jump()
+    {
+        body.linearVelocity = new UnityEngine.Vector2(body.linearVelocityX, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
