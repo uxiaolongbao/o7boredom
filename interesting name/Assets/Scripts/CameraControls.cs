19acfa52bd6e7c1a3d16c61dfa8e462f31c1a182
cyng1 <cynthiacggao@gmail.com>
2025-05-18 20:50:14 -0700

    added parallax. will need to fill those pits with bg. also slightly laggy. also player is like same height as blocks, that's my bad should prob fix

--- interesting name/Assets/Scripts/CameraControls.cs
+++ interesting name/Assets/Scripts/CameraControls.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
+
+    private float startPos, length;
+    public GameObject cam;
+    public float parallaxEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > startPos + length)
+        {
+            startPos += length;
+        } else if (movement < startPos - length)
+        {
+            startPos -= length;
+        }
     }
 }
