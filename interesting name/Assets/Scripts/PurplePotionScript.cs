4d6f07500c68349c202433ac19b536f7f5858b1c
Ri-Zh <zhzh38282@gmail.com>
2025-06-05 14:56:57 -0700

    purple potion

---
+++ interesting name/Assets/Scripts/PurplePotionScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PurplePotionScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+    if(other.gameObject.CompareTag("Player"))
+    {
+        Destroy(gameObject);
+        //speed?
+    }   
+    }  
+}
+
